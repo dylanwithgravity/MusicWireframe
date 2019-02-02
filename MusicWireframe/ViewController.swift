70902f0cfe0f063906564e8d56e91b99184cd7af
dylanwithgravity <dylanwithgravity@gmail.com>
2019-02-02 10:54:31 -0500

    Initial Commit

---
+++ MusicWireframe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Dylan Williamson on 2/2/19.
+//  Copyright © 2019 Dylan Williamson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
