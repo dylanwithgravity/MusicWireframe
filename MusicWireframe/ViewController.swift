171c9126ebaaff4448469b2e82138cdf57623ab6
dylanwithgravity <dylanwithgravity@gmail.com>
2019-02-02 12:09:44 -0500

    Implemented function to animate imageView when toggling play/pause

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isPlaying: Bool = true {
+        didSet {
+            if isPlaying {
+                playPauseButton.setImage(UIImage(named: "pause")!, for: .normal)
+            } else {
+                playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var albumImageView: UIView!
+    @IBOutlet weak var reverseBackground: UIView!
+    @IBOutlet weak var playPauseBackground: UIView!
+    @IBOutlet weak var forwardBackground: UIView!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        reverseBackground.layer.cornerRadius = 35.0
+        reverseBackground.clipsToBounds = true
+        reverseBackground.alpha = 0
+        
+        playPauseBackground.layer.cornerRadius = 35.0
+        playPauseBackground.clipsToBounds = true
+        playPauseBackground.alpha = 0
+        
+        forwardBackground.layer.cornerRadius = 35.0
+        forwardBackground.clipsToBounds = true
+        forwardBackground.alpha = 0
     }
+    
+    @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+        
+        if isPlaying {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            }
+        } else {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform.identity
+            }
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
 
 
 }
